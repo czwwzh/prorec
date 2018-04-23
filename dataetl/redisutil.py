eb9e6f6464f3e1afd568aec0f4e7b049277d3241
张昊 <zhanghao@epoque.shoes>
2018-04-23 15:15:14 +0800

    2018423prodrec-1

--- dataetl/redisutil.py
+++ dataetl/redisutil.py
@@ -1,48 +1,40 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+
+from configurationlocal import *
 import redis
-import time
-
-from dataetlconfiguration import *
-
 
 class Redis_db:
+
     RedisInfo = REDIS_CONNECT_INFO
     RedisConn = None
 
-    __RedisData = 'recommend_data'
-
-    def __init__(self):
+    def __init__(self,rdsdata = 'recommend_data'):
+        self.__RedisData = rdsdata
         self.link_redis()
 
-    def link_redis(self,num = 0):
+    def link_redis(self):
         conf = self.RedisInfo
-        # print('connect redis'),
+        ist = True
+        print('connect redis',)
         self.RedisConn = redis.Redis(**conf)
         try:
             self.RedisConn.ping()
-            # print('--->Success')
-            return True
+            print('--->Success')
         except redis.exceptions.ConnectionError as e:
-            # print('ERROR:'+str(e),'Redis')
-            time.sleep(1)
-            num+=1
-            if num < 2:
-                self.link_redis(num)
-            return False
+            print('ERROR:'+str(e),'Redis')
+            ist = False
+
+        return ist
 
     def GetList(self):
         try:
             self.RedisConn.ping()
             _,json = self.RedisConn.blpop(self.__RedisData)
         except TypeError as e:
-            # print('ERROR:' + str(e))
-            res = self.link_redis()
-            if res == False:
-                # print('GETLIST:CLOSE CONNECT REDIS')
-                return res
-            json = self.RedisConn.blpop(self.__RedisData)
+            print('ERROR:' + str(e))
+            json = None
         return json
 
     def RpushData(self,string):
@@ -50,11 +42,29 @@ class Redis_db:
             self.RedisConn.ping()
             self.RedisConn.rpush(self.__RedisData, string)
         except redis.exceptions.ResponseError as e:
-            # print('ERROR:' + str(e))
-            #self.__Obj.WriteLog('ERROR:' + str(e), 'Redis')
-            res = self.link_redis()
-            if res == False:
-                # print('GETLIST:CLOSE CONNECT REDIS')
-                #self.__Obj.WriteLog('GETLIST:CLOSE CONNECT REDIS', 'Redis')
-                return res
-            self.RedisConn.rpush(self.__RedisData, string)
+            print('ERROR:' + str(e))
+
+    def LenData(self):
+        llen = 0
+        try:
+            self.RedisConn.ping()
+            llen = self.RedisConn.llen(self.__RedisData)
+        except redis.exceptions.ResponseError as e:
+            print('ERROR:' + str(e))
+
+        return llen
+
+    def SetData(self, uuid, data=None, ist=False):
+        if data == None:
+            ist = True
+        try:
+            self.RedisConn.ping()
+        except redis.exceptions.ResponseError as e:
+            print('ERROR:' + str(e))
+
+        if ist == False:
+            self.RedisConn.set(uuid, data)
+        else:
+            res = self.RedisConn.get(uuid)
+            self.RedisConn.delete(uuid)
+            return res
