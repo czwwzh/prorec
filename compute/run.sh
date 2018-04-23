e7a7f4ed26c093f623d66752ba30965f060a18a7
张昊 <zhanghao@epoque.shoes>
2018-04-23 20:02:13 +0800

    e

--- compute/run.sh
+++ compute/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source /home/ec2-user/virtualenv36/bin/activate
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute1_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute2_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute3_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute4_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute5_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute6_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute7_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/wwwroot/py_ApiConter/log/modelcompute8_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute1_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute2_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute3_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute4_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute5_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute6_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute7_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/compute/modelcompute.py> /home/ec2-user/zhanghao/prodrec/modelcompute8_`date "+%Y-%m-%d"`.log 2>&1 &
--- dataetl/run.sh
+++ dataetl/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/virtualenv36/bin/activate
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/dataetl/transfer.py> /home/wwwroot/py_ApiConter/log/transfer_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/dataetl/read.py> /home/wwwroot/py_ApiConter/log/read_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/dataetl/transfer.py> /home/ec2-user/zhanghao/prodrec/dataetl/log/transfer_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup /usr/bin/python3.6 -u /home/ec2-user/zhanghao/prodrec/dataetl/read.py> /home/ec2-user/zhanghao/prodrec/dataetl/log/read_`date "+%Y-%m-%d"`.log 2>&1 &
