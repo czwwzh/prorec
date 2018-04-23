e65b4d3d62fc2712e1718779eea024a564a93134
张昊 <zhanghao@epoque.shoes>
2018-04-23 20:25:33 +0800

    e

--- dataetl/run.sh
+++ dataetl/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/virtualenv36/bin/activate
-nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/transfer.py> /home/ec2-user/zhanghao/prodrec/dataetl/log/transfer_`date "+%Y-%m-%d"`.log 2>&1 &
-nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/read.py> /home/ec2-user/zhanghao/prodrec/dataetl/log/read_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/transfer.py> /home/ec2-user/zhanghao/prodrec/log/dataetl/log/transfer_`date "+%Y-%m-%d"`.log 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/read.py> /home/ec2-user/zhanghao/prodrec/log/dataetl/log/read_`date "+%Y-%m-%d"`.log 2>&1 &
