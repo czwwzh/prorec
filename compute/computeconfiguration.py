596bd5237d29813d8fd8e0423a4c1ddbd95af056
zhanghao <zhanghao@epoque.shoes>
2018-05-09 16:06:52 +0800

    'redis'

--- compute/computeconfiguration.py
+++ compute/computeconfiguration.py
@@ -54,9 +54,11 @@ SIZESTANDARDSCALER3PATH = '/sizemodel-2/StandardScaler_lgbm_shoe_3'
 # local
 # REDIS_HOST = '192.168.17.110'
 # prod
-REDIS_HOST = '52.80.12.121'
+# REDIS_HOST = '52.80.12.121'
+REDIS_HOST = 'prod-sku-recommend.rawr9u.clustercfg.cnn1.cache.amazonaws.com.cn'
 REDIS_PORT = 6379
 REDIS_CONNECT_INFO = {'host':REDIS_HOST,'port':6379,'db':1}
+
 
 
 
