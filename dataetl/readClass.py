97333c70c2ec44ea7f598ef41cc8515e011ed7b3
zhanghao <zhanghao@epoque.shoes>
2018-05-09 17:04:47 +0800

    'redis'

--- dataetl/readClass.py
+++ dataetl/readClass.py
@@ -15,7 +15,8 @@ class readClass:
         my_rds = rds('recommend_data_msg')
         json_str = my_rds.SetData(uuid)
         json_str = json_str.decode()
-        print('1.data input time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+        # print('1.data input time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
         # get data one by one
         ist = True
@@ -36,30 +37,30 @@ class readClass:
         res = footdatasavemysql(key, sourcedata)
         # data dataetl
         if res > 0:
-            print('2.data save to mysql time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # print('2.data save to mysql time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
             # filter data which is not str type
             res1 = streamstr(key, sourcedata)
-            print('3.filter data which is not str type time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # print('3.filter data which is not str type time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             if res1 == False:
                 self.sendtowx(uuid)
                 return
 
             # filter data which is not json type
             res2 = streamjson(key, sourcedata)
-            print(res2)
-            print('4.filter data which is not json type time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # print(res2)
+            # print('4.filter data which is not json type time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             if res2 == False:
                 self.sendtowx(uuid)
                 return
 
             # transform data to json
             data = json.loads(sourcedata)
-            print('5.transform data to json time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # print('5.transform data to json time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
             # filter abnormal foot data
             res3 = footfilter(key, data)
-            print('6.filter abnormal foot data time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # print('6.filter abnormal foot data time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
             if res3 == False:
                 self.sendtowx(uuid)
                 return
@@ -67,12 +68,12 @@ class readClass:
                 # get foot data in demend
             data = getFootData(data)
             uuid = data[1]['UUID']
-            print('7.get foot data in demend time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+            # print('7.get foot data in demend time:  ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
             # get last data by shopno_sex and foot connect last
             data = footconnectlast(data)
-            print('8.get last data by shopno_sex and foot connect last time:  ' + time.strftime("%Y-%m-%d %H:%M:%S",
-                                                                                                time.localtime()))
+            # print('8.get last data by shopno_sex and foot connect last time:  ' + time.strftime("%Y-%m-%d %H:%M:%S",
+            #                                                                                     time.localtime()))
 
             # foot and last data dataetl save and send to redis
             res = footlastetlsave(data)
@@ -84,9 +85,9 @@ class readClass:
             else:
                 repetitivedatasave(uuid, sourcedata)
                 self.sendtowx(uuid)
-            print(
-                '9.foot and last data dataetl save to mysql and send to redis time:  ' + time.strftime("%Y-%m-%d %H:%M:%S",
-                                                                                                   time.localtime()))
+            # print(
+                # '9.foot and last data dataetl save to mysql and send to redis time:  ' + time.strftime("%Y-%m-%d %H:%M:%S",
+                #                                                                                    time.localtime()))
         else:
             repetitivedatasave(key, sourcedata)
             self.sendtowx(uuid)
