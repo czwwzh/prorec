ed63404b0612bcafbb7af85a5f8399902a00f6b0
zhanghao <zhanghao@epoque.shoes>
2018-06-01 13:50:52 +0800

    run

--- dataetl/run.sh
+++ dataetl/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/ec2-user/virtualenv36/bin/activate
-nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/transfer.py &
-nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/read.py &
+nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/transfer.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/read.py >/dev/null 2>&1 &
