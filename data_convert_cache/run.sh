d36b0b903cfd023a39aaf2ee2855a941da40ba0b
zhanghao <zhanghao@epoque.shoes>
2018-06-15 14:48:15 +0800

    run

--- data_convert_cache/run.sh
+++ data_convert_cache/run.sh
@@ -5,6 +5,14 @@ nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/kafka_redis.p
 
 nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
 nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
 
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
 nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
 nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
