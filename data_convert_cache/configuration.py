9dc619c803d0de3d5d2f8f4ade953026902f1abe
zhanghao <zhanghao@epoque.shoes>
2018-06-20 09:47:02 +0800

    test

--- data_convert_cache/configuration.py
+++ data_convert_cache/configuration.py
@@ -22,15 +22,15 @@ LOG_FILE_PATH_FUNC = '/home/ec2-user/zhanghao/log/func_'
 KAFKA_PROD_BROKERS = ['54.222.152.174:9092','54.222.195.114:9092','52.80.73.74:9092']
 KAFKA_PROD_FOOTTOPIC = 'footInfoProd'
 
-KAFKA_GROUP_ID = 'footInfoProdprod0615'
+KAFKA_GROUP_ID = 'footInfoProdTest0620'
 
 
 
 # redis configuration
 # test3
-REDIS_HOST = '52.80.137.153'
+# REDIS_HOST = '52.80.137.153'
 # online
-# REDIS_HOST = 'web-service-prod.rawr9u.ng.0001.cnn1.cache.amazonaws.com.cn'
+REDIS_HOST = 'web-service-prod.rawr9u.ng.0001.cnn1.cache.amazonaws.com.cn'
 REDIS_PORT = 6379
 REDIS_CONNECT_INFO = {'host':REDIS_HOST,'port':6379,'db':0}
 # kafka 数据转入redis中的队列名 uuid
@@ -46,9 +46,9 @@ REDIS_LIST_COMPUTE_RESULT = 'redis_list_compute_result'
 # return url
 # test
 # RETURN_PORT_URL = 'http://test.epoque.cn/Testbigdata'
-# RETURN_PORT_URL = 'http://epoque.epoque.cn/bdp/Bdsendmsgman'
+RETURN_PORT_URL = 'http://epoque.epoque.cn/bdp/Bdsendmsgman'
 # prod
-RETURN_PORT_URL = 'http://epoque.epoque.cn/bdp/Bdsendmsg'
+# RETURN_PORT_URL = 'http://epoque.epoque.cn/bdp/Bdsendmsg'
 
 # RETURN_PORT_URL = 'http://test.epoque.cn/Testbigdata'
 # data_compute completed return host
---
+++ data_convert_cache/run1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
