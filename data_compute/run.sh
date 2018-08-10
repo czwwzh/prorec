d944d95c7de4581b8991d7df8cd2eb7a6e41838d
zhanghao <zhanghao@epoque.shoes>
2018-08-10 17:12:49 +0800

    男女模型整版本 线上环境修改

--- data_compute/run.sh
+++ data_compute/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source /home/ec2-user/virtualenv36/bin/activate
-nohup python -u /home/ec2-user/zhanghao/prodrec/compute/compute.py >/dev/null 2>&1 &
+nohup python -u /home/ec2-user/zhanghao/prodrec/data_compute/compute.py >/dev/null 2>&1 &
