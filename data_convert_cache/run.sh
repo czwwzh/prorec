9a3151f123887916b929a552fcda1b06e7168785
zhanghao <zhanghao@epoque.shoes>
2018-10-15 13:46:39 +0800

    aws环境迁移

--- data_convert_cache/run.sh
+++ data_convert_cache/run.sh
@@ -2,7 +2,7 @@
 #source /home/ec2-user/virtualenv36/bin/activate
 source /root/virtualenv36/bin/activate
 nohup python -u /root/projects/recommend/prodrec/data_convert_cache/kafka_redis.py >/dev/null 2>&1 &
-#nohup python -u /root/projects/recommend/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
-#nohup python -u /root/projects/recommend/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+nohup python -u /root/projects/recommend/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+nohup python -u /root/projects/recommend/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
 
 
