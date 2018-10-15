af8e1e0ac60698eef5c0f058548b84aed2dbbb5a
zhanghao <zhanghao@epoque.shoes>
2018-10-15 11:28:36 +0800

    aws环境迁移

--- data_convert_cache/run.sh
+++ data_convert_cache/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #source /home/ec2-user/virtualenv36/bin/activate
-source /root/apps/virtualenv36/bin/activate
-nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/kafka_redis.py >/dev/null 2>&1 &
-#nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
-#nohup python -u /home/ec2-user/zhanghao/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+source /root/virtualenv36/bin/activate
+nohup python -u /root/projects/recommend/prodrec/data_convert_cache/kafka_redis.py >/dev/null 2>&1 &
+#nohup python -u /root/projects/recommend/prodrec/data_convert_cache/return_abnormal_data.py >/dev/null 2>&1 &
+#nohup python -u /root/projects/recommend/prodrec/data_convert_cache/return_normal_data.py >/dev/null 2>&1 &
+
+
--- dataetl/run.sh
+++ dataetl/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/ec2-user/virtualenv36/bin/activate
-nohup python -u /home/ec2-user/zhanghao/prodrec/dataetl/etl.py >/dev/null 2>&1 &
+source /root/virtualenv36/bin/activate
+nohup python -u /root/projects/recommend/prodrec/dataetl/etl.py >/dev/null 2>&1 &
